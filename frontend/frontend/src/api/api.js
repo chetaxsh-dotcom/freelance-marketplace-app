702cdfad2cd788e9e3d98ead8ae3cb67c5221883
Chetan <chetansabili@gmail.com>
2026-05-11 18:25:30 +0530

    fixed production api url

--- frontend/frontend/src/api/api.js
+++ frontend/frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://freelance-marketplace-app.onrender.com"
+  baseURL: "https://freelance-marketplace-app.onrender.com/api"
 });
 
 //  AUTO TOKEN
