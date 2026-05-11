3dc7e0639b2b710c1e3ff386e67606f371dcc41c
Chetan <chetansabili@gmail.com>
2026-05-11 18:10:32 +0530

    fixed deployed api url

--- frontend/frontend/src/api/api.js
+++ frontend/frontend/src/api/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://freelance-marketplace-app.onrender.com"
 });
 
-// 🔥 AUTO TOKEN
+//  AUTO TOKEN
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) {
